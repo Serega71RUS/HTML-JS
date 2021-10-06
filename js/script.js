5a1cc3bc7db668fb4b259a404368eb3e4194d469
Serega71RUS <serega71rus@gmail.com>
2021-10-06 14:07:51 +0300

    My answer to the task#5 030

--- js/script.js
+++ js/script.js
@@ -21,6 +21,37 @@ const movieDB = {
         "Ла-ла лэнд",
         "Одержимость",
         "Скотт Пилигрим против..."
-    ]
+    ],
+    changeListMovies: function() {
+        //promo__interactive-list
+        //promo__interactive-item
+        let items = document.querySelectorAll('.promo__interactive-item');
+        //let i = 0;
+        items.forEach((element,i) => {
+            element.textContent = `${i+1}. ${movieDB.movies.sort()[i]}`;
+            //i++;
+        });
+    }
 };
 
+const modifPage = {
+    removeAdd: function() {
+        const addBlock = document.querySelectorAll('.promo__adv img');
+        addBlock.forEach(element => {
+            element.remove();
+        });
+    },
+    modifGenre: function() {
+        let genre = document.querySelector('.promo__genre');
+        genre.textContent = 'Драма';
+    },
+    changeBackground: function() {
+        let bg = document.querySelector('.promo__bg');
+        bg.style.cssText = 'background: url(img/bg.jpg) center (center / cover) no-repeat';
+    }
+};
+
+modifPage.removeAdd();
+modifPage.modifGenre();
+modifPage.changeBackground();
+movieDB.changeListMovies();
