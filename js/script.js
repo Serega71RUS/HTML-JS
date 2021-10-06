b0a3c55a963cb3392ed5d5a0a71cbd7278286955
Serega71RUS <serega71rus@gmail.com>
2021-10-06 18:04:07 +0300

    My answer to the task#5, 4/5 tasks completed

--- js/script.js
+++ js/script.js
@@ -1,57 +1,125 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ],
-    changeListMovies: function() {
-        //promo__interactive-list
-        //promo__interactive-item
-        let items = document.querySelectorAll('.promo__interactive-item');
-        //let i = 0;
-        items.forEach((element,i) => {
-            element.textContent = `${i+1}. ${movieDB.movies.sort()[i]}`;
-            //i++;
+document.addEventListener('DOMContentLoaded', () => {
+
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ],
+        changeListMovies: function() {
+            //promo__interactive-list
+            //promo__interactive-item
+            let itemlist = document.querySelector('.promo__interactive-list');
+            let items = document.querySelector('.promo__interactive-item');
+            itemlist.innerHTML = "";
+            this.movies.sort();
+            this.movies.forEach((element,i) => {
+                itemlist.innerHTML += `<li class="promo__interactive-item">${i+1} ${this.movies[i]}<div class="delete"></div></li>`;
+            });
+            //let i = 0;
+            // items.forEach((element,i) => {
+            //     element.textContent = `${i+1}. ${movieDB.movies.sort()[i]}`;
+            //     //i++;
+            // });
+        },
+
+    };
+
+    const modifPage = {
+        removeAdd: function() {
+            const addBlock = document.querySelectorAll('.promo__adv img');
+            addBlock.forEach(element => {
+                element.remove();
+            });
+        },
+        modifGenre: function() {
+            let genre = document.querySelector('.promo__genre');
+            genre.textContent = 'Драма';
+        },
+        changeBackground: function() {
+            let bg = document.querySelector('.promo__bg');
+            bg.style.cssText = 'background: url(img/bg.jpg) center (center / cover) no-repeat';
+        }
+    };
+    function StringLenght(str) {
+        let i = 0;
+        while (str[i] != null){
+            i++;
+        }
+        return i;
+    }
+
+    //Events
+    //Add Movies
+    function AddMovies(){
+        const btnAdd = document.querySelector('.promo__interactive .add button');
+        const textBox = document.querySelector('.promo__interactive .add input');
+        const checkbox = document.querySelector('[type="checkbox"]');
+        btnAdd.addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log(textBox.value);
+            if(textBox.value != ""){
+                if(StringLenght(textBox.value) > 21){
+                    if(checkbox.checked){
+                        console.log(`Добавляем любимый фильм`);
+                        textBox.value = textBox.value.substring(0, 21) + "...";
+                        movieDB.movies.push(textBox.value);
+                        movieDB.changeListMovies();
+                    }
+                    else{
+                        movieDB.movies.push(textBox.value);
+                        movieDB.changeListMovies();
+                    }
+                }
+                else{
+                    if(checkbox.checked){
+                        console.log(`Добавляем любимый фильм`);
+                        textBox.value = textBox.value.substring(0, 21) + "...";
+                        movieDB.movies.push(textBox.value);
+                        movieDB.changeListMovies();
+                    }
+                    else{
+                        movieDB.movies.push(textBox.value);
+                        movieDB.changeListMovies();
+                    }
+                }
+                textBox.value = null;
+                checkbox.checked = false;
+                
+            }
         });
     }
-};
 
-const modifPage = {
-    removeAdd: function() {
-        const addBlock = document.querySelectorAll('.promo__adv img');
-        addBlock.forEach(element => {
-            element.remove();
+
+    //Delete Movies
+    const delCart = document.querySelectorAll('.delete');
+    //console.log(delCart);
+    const delEvent = (e) => {
+        console.log(e.target);
+
+    };
+    delCart.forEach(element => {
+        element.addEventListener('click', (e) => {
+            console.log(e.target);
         });
-    },
-    modifGenre: function() {
-        let genre = document.querySelector('.promo__genre');
-        genre.textContent = 'Драма';
-    },
-    changeBackground: function() {
-        let bg = document.querySelector('.promo__bg');
-        bg.style.cssText = 'background: url(img/bg.jpg) center (center / cover) no-repeat';
-    }
-};
+    });
+    console.log(delCart);
+    let mass = [];
+    // delCart.forEach((element,i) => {
+    //     console.log(element.parentElement);
+    //     mass[i] = element.parentElement.textContent;
+    // });
+    console.log(mass);
 
-modifPage.removeAdd();
-modifPage.modifGenre();
-modifPage.changeBackground();
-movieDB.changeListMovies();
+
+
+    modifPage.removeAdd();
+    modifPage.modifGenre();
+    modifPage.changeBackground();
+    movieDB.changeListMovies();
+    AddMovies();
+});
