bf3bed8388c248baa7f7e5885385c7789aeaf985
Serega71RUS <serega71rus@gmail.com>
2021-10-07 10:46:53 +0300

    My answer to the task#5 + teacher correction

--- js/script.js
+++ js/script.js
@@ -10,21 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
             "Одержимость",
             "Скотт Пилигрим против..."
         ],
-        changeListMovies: function() {
+        changeListMovies: function(movies) {
             //promo__interactive-list
             //promo__interactive-item
             let itemlist = document.querySelector('.promo__interactive-list');
-            let items = document.querySelector('.promo__interactive-item');
+            //let items = document.querySelector('.promo__interactive-item');
             itemlist.innerHTML = "";
-            this.movies.sort();
-            this.movies.forEach((element,i) => {
-                itemlist.innerHTML += `<li class="promo__interactive-item">${i+1} ${this.movies[i]}<div class="delete"></div></li>`;
+            movies.sort();
+            movies.forEach((element,i) => {
+                itemlist.innerHTML += `<li class="promo__interactive-item">${i+1} ${movies[i]}<div class="delete"></div></li>`;
             });
-            //let i = 0;
-            // items.forEach((element,i) => {
-            //     element.textContent = `${i+1}. ${movieDB.movies.sort()[i]}`;
-            //     //i++;
-            // });
+            //Delete Movies
+            document.querySelectorAll('.delete').forEach((element, i) => {
+                element.addEventListener('click', (e) => {
+                    element.parentElement.remove();
+                    movies.splice(i,1);
+                    this.changeListMovies(movies);
+                });
+            });
         },
 
     };
@@ -36,90 +39,61 @@ document.addEventListener('DOMContentLoaded', () => {
                 element.remove();
             });
         },
-        modifGenre: function() {
+        modifGenre: function(str) {
             let genre = document.querySelector('.promo__genre');
-            genre.textContent = 'Драма';
+            genre.textContent = str;
         },
-        changeBackground: function() {
+        changeBackground: function(path) {
             let bg = document.querySelector('.promo__bg');
-            bg.style.cssText = 'background: url(img/bg.jpg) center (center / cover) no-repeat';
+            //bg.style.cssText = 'background: url(img/bg.jpg) center (center / cover) no-repeat';
+            bg.style.backgroundImage = (`url(${path})`);
         }
     };
-    function StringLenght(str) {
-        let i = 0;
-        while (str[i] != null){
-            i++;
-        }
-        return i;
-    }
 
     //Events
     //Add Movies
-    function AddMovies(){
-        const btnAdd = document.querySelector('.promo__interactive .add button');
-        const textBox = document.querySelector('.promo__interactive .add input');
-        const checkbox = document.querySelector('[type="checkbox"]');
-        btnAdd.addEventListener('click', (e) => {
-            e.preventDefault();
-            console.log(textBox.value);
-            if(textBox.value != ""){
-                if(StringLenght(textBox.value) > 21){
-                    if(checkbox.checked){
-                        console.log(`Добавляем любимый фильм`);
-                        textBox.value = textBox.value.substring(0, 21) + "...";
-                        movieDB.movies.push(textBox.value);
-                        movieDB.changeListMovies();
-                    }
-                    else{
-                        movieDB.movies.push(textBox.value);
-                        movieDB.changeListMovies();
-                    }
+    const addMovies = {
+        addMovie: function(movies) {
+            const btnAdd = document.querySelector('.promo__interactive .add button');
+            const textBox = document.querySelector('.promo__interactive .add input');
+            const checkbox = document.querySelector('[type="checkbox"]');
+            btnAdd.addEventListener('click', (e) => {
+                e.preventDefault();
+                if(textBox.value){
+                    textBox.value = this.checkLength(textBox.value);
+                    this.checkboxFunc(checkbox, textBox.value);
+                    movieDB.changeListMovies(movies);
+                    textBox.value = null;
+                    checkbox.checked = false;
+                    
                 }
-                else{
-                    if(checkbox.checked){
-                        console.log(`Добавляем любимый фильм`);
-                        textBox.value = textBox.value.substring(0, 21) + "...";
-                        movieDB.movies.push(textBox.value);
-                        movieDB.changeListMovies();
-                    }
-                    else{
-                        movieDB.movies.push(textBox.value);
-                        movieDB.changeListMovies();
-                    }
-                }
-                textBox.value = null;
-                checkbox.checked = false;
-                
+            });
+        },
+        checkboxFunc: function(checkbox, filmName) {
+            if(checkbox.checked){
+                console.log(`Добавляем любимый фильм`);
+                movieDB.movies.push(filmName);
+                return true;
             }
-        });
-    }
-
-
-    //Delete Movies
-    const delCart = document.querySelectorAll('.delete');
-    //console.log(delCart);
-    const delEvent = (e) => {
-        console.log(e.target);
-
+            else{
+                movieDB.movies.push(filmName);
+                return false;
+            }
+        },
+        checkLength: function(filmName) {
+            if(filmName.length > 21){
+                filmName = filmName.substring(0, 21) + "...";
+                return filmName;
+            }
+            else{
+                return filmName;
+            }
+        }
     };
-    delCart.forEach(element => {
-        element.addEventListener('click', (e) => {
-            console.log(e.target);
-        });
-    });
-    console.log(delCart);
-    let mass = [];
-    // delCart.forEach((element,i) => {
-    //     console.log(element.parentElement);
-    //     mass[i] = element.parentElement.textContent;
-    // });
-    console.log(mass);
-
-
 
     modifPage.removeAdd();
-    modifPage.modifGenre();
-    modifPage.changeBackground();
-    movieDB.changeListMovies();
-    AddMovies();
+    modifPage.modifGenre("Драма");
+    modifPage.changeBackground("img/bg.jpg");
+    addMovies.addMovie(movieDB.movies);
+    movieDB.changeListMovies(movieDB.movies);
 });
